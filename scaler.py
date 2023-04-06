7d2c3da371259bfd60c54b42a702c804374f20dd
boribs <christumer@gmail.com>
2023-04-06 13:58:46 -0600

    Scaler.py
    Para escalar imágenes

---
+++ scaler.py
@@ -0,0 +1,80 @@
+import sys, os, argparse
+from PIL import Image
+
+# DEFAULT_RES = (3024, 4032)
+
+def resize(
+        path: str,
+        scale: int | None = 1,
+        res: tuple[int] | None = None,
+        replace: bool = True
+    ):
+    img = Image.open(path)
+    filename = path[path.rfind('/') + 1:]
+
+    size = res if res else (img.size[0] * scale, img.size[1] * scale)
+    outname = filename if replace else 'c-'+ filename
+    outpath = path[:path.find('/') + 1] + outname
+
+    img.thumbnail(size)
+    img.save(outpath)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='Image scaler!',
+        usage='scaler.py [OPTIONS] folder(s)',
+        description='Utility to scale images.'
+    )
+    parser.add_argument(
+        '-s', '--scale',
+        default=None,
+        nargs='?',
+    )
+    parser.add_argument(
+        '--width',
+        default=None,
+        nargs='?',
+    )
+    parser.add_argument(
+        '--height',
+        default=None,
+        nargs='?',
+    )
+    parser.add_argument(
+        'folders',
+        nargs='*'
+    )
+    parser.add_argument(
+        '--no-replace',
+        action='store_true'
+    )
+    args = parser.parse_args()
+
+    if not args.width and not args.height and not args.scale:
+        print('Must specify either width and height or scale.')
+        exit(1)
+
+    if not(args.height and args.width) and not (args.height == None and args.width == None):
+        print('Must specify both width and height.')
+        exit(1)
+
+    if args.scale and args.width:
+        print('Must specify only resolution or scale.')
+        exit(1)
+
+    if not args.scale and not args.width and not args.height:
+        print('Must specify some way of resizing the image!')
+        exit(1)
+
+    res = (float(args.height), float(args.width)) if args.height else None
+    scale = float(args.scale) if args.scale else None
+
+    for folder in args.folders:
+        for img in os.listdir(folder):
+            print(f'Resizing {img}')
+            resize(
+                folder + '/' + img,
+                scale=scale,
+                res=res,
+                replace=not args.no_replace
+            )
