2b31641e9cbd3d7f60de20e960896467dc068999
boribs <christumer@gmail.com>
2023-04-06 15:04:26 -0600

    Más integración?

--- raspi/arenito.py
+++ raspi/arenito.py
@@ -1,12 +1,48 @@
-import serial, time
+import serial
+import cv2
+import sys
 from random import randint
+from tflite_support.task import core
+from tflite_support.task import processor
+from tflite_support.task import vision
 
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1) # Encontrar esto automáticamente?
 
-while True:
-    msg = ser.seradline().decode('utf-8')
-    if msg == 'latas':
-        print('Enviando latas')
-        time.sleep(randint(1, 3))
-        ser.write(bytes('ok', 'utf-8'))
-        print('Enviadas')
+def crea_lista(detecciones) -> str:
+    salida = "{"
+    for det in detecciones.detections:
+        bbox = det.bounding_box
+        a = (bbox.origin_x, bbox.origin_y)
+        b = (a[0] + bbox.width, a[1] + bbox.height)
+        salida += f"[{a[0]},{a[1]},{b[0]},{b[1]},]"
+
+    return salida + "}"
+
+
+def detecta_latas():
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ok, img = cap.read()
+        if not ok:
+            sys.exit("Error leyendo la cámara.")
+
+        # Aquí las cosas de la detección
+
+        if cv2.waitKey(1) == 27:
+            break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        return None
+
+
+def main():
+    ser = serial.Serial(
+        "/dev/ttyUSB0", 115200, timeout=0.1
+    )  # Encontrar esto automáticamente?
+
+    while True:
+        msg = ser.seradline().decode("utf-8")
+        if msg == "latas":
+            detecciones = detecta_latas()
+            ser.write(bytes(crea_lista(detecciones), "utf-8"))
