45dd74fc112bdc0b52e6036a7538f1239b7252ee
boribs <christumer@gmail.com>
2023-04-06 11:27:11 -0600

    Comunicación con arduino

--- arenito.py
+++ arenito.py
@@ -1,4 +1,12 @@
 import serial, time
+from random import randint
 
-ser = serial.Serial('/dev/ttyUSB0') # Encontrar esto automáticamente?
-ser.write(bytes('8', 'utf-8'))
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1) # Encontrar esto automáticamente?
+
+while True:
+    msg = ser.seradline().decode('utf-8')
+    if msg == 'latas':
+        print('Enviando latas')
+        time.sleep(randint(1, 3))
+        ser.write(bytes('ok', 'utf-8'))
+        print('Enviadas')
