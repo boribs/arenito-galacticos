42892f19ce77ad35165698fe95dd936f50c30632
boribs <christumer@gmail.com>
2023-04-10 17:39:24 -0600

    Almacenamiento de latas - mensaje serial
    El arduino guardará las latas que haya recibido a través del bus serial en un arreglo.
    
    El mensaje tiene el formato {x1,y1,x2,y2,...,xn,yn} donde cada par corresponde al centro de una detección.

--- arduino/Motores-v1/Motores-v1.ino
+++ arduino/Motores-v1/Motores-v1.ino
@@ -7,6 +7,15 @@ int motIa = 8;
 int motDb = 7;
 int motDa = 6;
 
+typedef struct lata {
+  int x;
+  int y;
+} lata;
+
+const int MAX_LATAS = 20;
+lata detectadas[MAX_LATAS] = { 0 };
+int eleigda = -1; // -1 = no se ha elegido ninguna
+
 void setup() {
   pinMode(motIa, OUTPUT);
   pinMode(motIb, OUTPUT);
@@ -18,7 +27,54 @@ void loop() {
   
 }
 
-void avanza () {
+/*
+ * Decodifica el mensaje enviado por el puerto serial
+ * que contiene los puntos medios de las detecciones
+ * de las latas.
+ *
+ * Ejemplo: {x1,y1,x2,y2,...,}
+ *
+ * Todos los valores son enteros positivos separados
+ * por comas. El último valor también debe llevar una
+ * coma al final (antes de }).
+ *
+ * Regresa el número de latas encontradas o -1 cuando
+ * ocurrió algún error.
+ */
+int descifraLatas() {
+  String msg = Serial.readString();
+  msg.trim();
+  size_t msg_len = msg.length();
+
+  if (msg[0] != '{' || msg[msg_len - 1] != '}') {
+    // Los delimitadores son incorrectos:
+    // no empieza con { ni termina con }
+    return -1;
+  }
+
+  int num_latas = 0;
+  String num = "";
+  bool set_x = false;
+
+  for (int i = 0; i < msg_len - 1; ++i) {
+    if (isDigit(msg[i])) {
+      num += msg[i];
+    } else if (msg[i] == ',') {
+      if (!set_x) {
+        detectadas[num_latas].x = num.toInt();
+        num = "";
+      } else {
+        detectadas[num_latas].y = num.toInt();
+        num = "";
+        num_latas++;
+      }
+      set_x = !set_x;
+    }
+  }
+
+  return num_latas;
+}
+
   digitalWrite(motIa, HIGH);
   digitalWrite(motIb, LOW);
   digitalWrite(motDa, HIGH);
