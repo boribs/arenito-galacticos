01363247779402a937ab35207d4cda16d4f4fbf9
boribs <christumer@gmail.com>
2023-04-12 16:45:46 -0600

    A script to clean up images

---
+++ utils/cleanup.py
@@ -0,0 +1,25 @@
+# A little program to delete those images that dont have
+# their respective .xml
+
+# python cleanup.py folder1 folder2 ...
+
+import sys, os
+
+for folder in sys.argv[1:]:
+    files = os.listdir(folder)
+    images = list(filter(lambda f: f.endswith('.JPG') or f.endswith('.jpg'), files))
+    xmls = list(filter(lambda f: f.endswith('.xml'), files))
+    delete = []
+
+    for img in images:
+        filename = img[:img.rfind('.')] + '.xml'
+
+        if filename not in xmls:
+            delete.append(os.path.join(folder, img))
+
+    if delete:
+        cmd = f'rm ' + ' '.join(delete)
+        os.system(cmd)
+
+        for d in delete:
+            print('Deleted ' + d)
