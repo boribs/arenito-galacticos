8176120080d4942a1aa325a0293a076ebe2a1613
boribs <christumer@gmail.com>
2023-04-12 18:50:14 -0600

    Adición de temporizador al colector de imágenes

--- utils/imagecollection.py
+++ utils/imagecollection.py
@@ -1,28 +1,18 @@
 # My laptop can't really handle notebooks, so I'm doing it here.
 
-import cv2, uuid, os, time, sys
+import cv2, uuid, argparse, time
 
-IMAGES_PATH = ''
-IMAGES_PATH = os.path.join('Users', 'boristoteles', 'Documents', 'tmr23', 'arenito', 'images')
 SCALE = 1 / 2
 
-# if 'path' in sys.argv:
-#     if not os.path.exists(IMAGES_PATH):
-#         if os.name == 'posix':
-#             os.system(f'mkdir -p {IMAGES_PATH}')
-#         if os.name == 'nt':
-#             os.system(f'mkdir {IMAGES_PATH}')
-
-#     for label in labels:
-#         path = os.path.join(IMAGES_PATH, label)
-#         if not os.path.exists(path):
-#             os.system(f'mkdir {path}')
-
-if __name__ == '__main__':
+def main():
     cap = cv2.VideoCapture(0)
 
-    while True:
-        ret, frame = cap.read()
+    while cap.isOpened():
+        ok, frame = cap.read()
+
+        if not ok:
+            print('Error comunicating with camera.')
+            exit(1)
 
         size = (int(frame.shape[1] * SCALE), int(frame.shape[0] * SCALE))
         frame = cv2.resize(frame, size)
@@ -34,8 +24,51 @@ if __name__ == '__main__':
             cv2.imwrite(imgname, frame)
             print(f'guardando {imgname}')
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == 27:
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+def main_timed(timer: int): # timed?
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ok, frame = cap.read()
+
+        if not ok:
+            print('Error comunicating with camera.')
+            exit(1)
+
+        size = (int(frame.shape[1] * SCALE), int(frame.shape[0] * SCALE))
+        frame = cv2.resize(frame, size)
+
+        cv2.imshow('frame', frame)
+
+        imgname = '{}.jpg'.format(str(uuid.uuid1()))
+        cv2.imwrite(imgname, frame)
+        print(f'guardando {imgname}')
+
+        time.sleep(timer)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--timer',
+        type=float,
+        default=0,
+    )
+
+    args = parser.parse_args()
+
+    if args.timer == 0:
+        main()
+    else:
+        main_timed(args.timer)
