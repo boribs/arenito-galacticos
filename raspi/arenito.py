bf16dfcf32003b3316b82fa833debf50a420f63b
boribs <christumer@gmail.com>
2023-04-12 18:17:14 -0600

    Actualización de resolución de imagen
    Basada en el escalado, tomando en cuenta la cámara.

--- raspi/arenito.py
+++ raspi/arenito.py
@@ -7,6 +7,8 @@ from tflite_support.task import core
 from tflite_support.task import processor
 from tflite_support.task import vision
 
+RES_X = 640
+RES_Y = 380
 CENTRO_INF = (RES_X // 2, RES_Y)
 MIN_PX_WATER = 50
 
