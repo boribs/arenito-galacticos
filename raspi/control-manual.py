699c7ca7dee5df15cc7ff042528a7e43ed706901
boribs <christumer@gmail.com>
2023-04-17 14:26:15 -0600

    Script de control manual.
    Por ahora solo controla el movimiento.

---
+++ raspi/control-manual.py
@@ -0,0 +1,113 @@
+import serial
+import argparse
+
+# El movimiento del arduino depende de las detecciones.
+# Actualmente, si tiene una lata "en frente", avanza hacia adelante.
+# Si la tiene a un lado, gira hacia ella.
+#
+# Este script simula haber encontrado detecciones, permitiendo controlar
+# manualmente el movimiento del arenito.
+#
+# Se considera "en frente" aquellas detecciones que, en el plano horizontal,
+# se encuentren en el rango de RES_X / 2 ± MARGEN_X.
+# RES_X y MARGEN_X están definidos en arenito.py, con el objetivo de
+# reducir los archivos a modificar para realizar cambios
+
+from arenito import RES_X, RES_Y, MARGEN_X
+
+CENTRO = RES_X // 2
+LIMITES = (CENTRO - MARGEN_X, CENTRO + MARGEN_X)
+
+def lerp(a: int, b: int, t: int):
+    return int(a + ((b - a) * (t / 100)))
+
+def adelante(ser: serial.Serial, perc: int | None):
+    if perc != None:
+        ser.write(bytes(
+            f'{{{CENTRO},{lerp(0, RES_Y, perc)},}}'
+            'utf-8'
+        ))
+    else:
+        ser.write(bytes('{' + str(CENTRO) + ',200,}', 'utf-8'))
+
+def izquierda(ser: serial.Serial, perc: int | None):
+    if perc != None:
+        ser.write(bytes(
+            f'{{{LIMITES[0] - lerp(0, LIMITES[0], perc)},200,}}'
+            'utf-8'
+        ))
+    else:
+        ser.write(bytes('{0,200,}', 'utf-8'))
+
+def derecha(ser: serial.Serial, perc: int | None):
+    if perc != None:
+        ser.write(bytes(
+            f'{{{lerp(LIMITES[1], RES_X, perc)},200,}}'
+            'utf-8'
+        ))
+    else:
+        ser.write(bytes('{' + str(RES_X) + ',200,}', 'utf-8'))
+
+def main(port: str, baudrate: int, timeout: float):
+    print('Iniciando control manual...')
+    ser = serial.Serial(port, baudrate, timeout=timeout)
+
+    while True:
+        try:
+            perc = None
+            dire = None
+            cmd = input('> ').strip().split()
+
+            if len(cmd) == 2:
+                dire, perc = cmd
+                perc = int(perc)
+
+                if perc > 100 or perc < 1:
+                    raise Exception('Porcentaje debe estar entre 1 y 100.')
+
+            elif len(cmd) == 1:
+                dire = cmd[0]
+            else:
+                raise Exception('Sintaxis inválida.')
+
+            dire = dire.lower()
+            if dire in ('exit', 'q', 'quit', 'salir', 's', 'bye'):
+                raise KeyboardInterrupt
+
+            # TODO: Comando para tomar foto?
+            if dire == 'a':
+                adelante(ser, perc)
+            elif dire == 'i':
+                izquierda(ser, perc)
+            elif dire == 'd':
+                derecha(ser, perc)
+            else:
+                raise Exception('Comando inválido.')
+
+        except KeyboardInterrupt:
+            print('\nSaliendo')
+            break
+
+        except Exception as e:
+            print(f'{e}\nRecuerda: [direccion] [porcentaje?]\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--port',
+        type=str,
+        default='/dev/ttyUSB0',
+    )
+    parser.add_argument(
+        '--baudrate',
+        type=int,
+        default=115200,
+    )
+    parser.add_argument(
+        '--timeout',
+        type=float,
+        default=0.1,
+    )
+    args = parser.parse_args()
+
+    main(args.port, args.baudrate, args.timeout)
