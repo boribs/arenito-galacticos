b65eec3125d704661a5d8ae639bdb1a5ce36aa88
boribs <christumer@gmail.com>
2023-04-27 18:58:32 -0600

    Considera el agua para determinar si una lata es accesible

--- raspi/arenito.py
+++ raspi/arenito.py
@@ -5,17 +5,38 @@ import time
 import argparse
 import numpy as np
 
+MARGEN_X = 35 # Aquí para no tener que
+              # modificarlo en más partes
+MIN_PX_WATER = 50
 
-def process_img(img: np.ndarray) -> np.ndarray:
-    lower = np.array([0, 0, 69])
-    upper = np.array([175, 255, 255])
+AZUL_LI = np.array([75, 160, 88], np.uint8)
+AZUL_LS = np.array([179, 255, 255], np.uint8)
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower, upper)
+def reachable(img: np.ndarray, det: tuple[int]) -> bool:
+    """
+    Determines if a detection is reachable.
+    Returns true if possible, otherwise false.
+    """
 
-    return mask
+    RES_Y, RES_X, _ = img.shape
+    CENTRO_INF = (RES_X // 2, RES_Y)
 
-def find_blobs(mask: np.ndarray) -> np.ndarray:
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(img_hsv, AZUL_LI, AZUL_LS)
+
+    line = np.zeros(shape=mask.shape, dtype=np.uint8)
+    cv2.line(line, CENTRO_INF, det, (255, 255, 255), thickness=10)
+
+    cross = cv2.bitwise_and(mask, line)
+    white_px = np.count_nonzero(cross)
+
+    cv2.imwrite('w.jpg', mask)
+    cv2.imwrite('x.jpg', line)
+    cv2.imwrite('y.jpg', cross)
+
+    return white_px < MIN_PX_WATER
+
+def find_blobs(img: np.ndarray) -> np.ndarray:
     params = cv2.SimpleBlobDetector_Params()
     params.filterByArea = True
     params.minArea = 500
@@ -24,9 +45,21 @@ def find_blobs(mask: np.ndarray) -> np.ndarray:
     params.filterByConvexity = False
     params.filterByInertia = False
 
+    lower = np.array([0, 0, 69])
+    upper = np.array([175, 255, 255])
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(mask)
-    im_with_keypoints = cv2.drawKeypoints(mask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    for k in keypoints:
+        det = tuple(map(int, k.pt))
+        if reachable(img, det):
+            cv2.circle(im_with_keypoints, det, 10, (255,0,0), 10)
+
     return im_with_keypoints
 
 def main():
