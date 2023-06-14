1b361e8d642499134a0b0ec34f580d46f3714a63
boribs <christumer@gmail.com>
2023-06-13 23:30:57 -0600

    Adición: Lectura de OBJ
    Se agregó un plugin para leer modelos .obj.

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,8 +1,10 @@
 use bevy::prelude::*;
+use bevy_obj::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(ObjPlugin)
         .add_startup_system(setup)
         .run();
 }
@@ -12,25 +14,47 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>
 ) {
-    // plane
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(5.0).into()),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
-    // cube
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        mesh: asset_server.load("arenito.obj"),
         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
-    // light
+    commands.spawn(PbrBundle {
+        mesh: asset_server.load("rueda.obj"),
+        material: materials.add(Color::rgb(0.8, 0.3, 0.6).into()),
+        transform: Transform::from_xyz(0.5, 0.5, 0.85),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: asset_server.load("rueda.obj"),
+        material: materials.add(Color::rgb(0.8, 0.3, 0.6).into()),
+        transform: Transform::from_xyz(-0.5, 0.5, 0.85),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: asset_server.load("rueda.obj"),
+        material: materials.add(Color::rgb(0.8, 0.3, 0.6).into()),
+        transform: Transform::from_xyz(0.5, 0.5, -0.85),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: asset_server.load("rueda.obj"),
+        material: materials.add(Color::rgb(0.8, 0.3, 0.6).into()),
+        transform: Transform::from_xyz(-0.5, 0.5, -0.85),
+        ..default()
+    });
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
-            shadows_enabled: true,
+            shadows_enabled: false,
             ..default()
         },
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
@@ -38,7 +62,8 @@ fn setup(
     });
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(2.0, 2.5, 7.0)
+                        .looking_at(Vec3::new(0.0, 0.0, -7.0), Vec3::Y),
         ..default()
     });
 }
