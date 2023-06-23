8fd72e3a4a2cd3555c5cc640f573eefd24d52d73
boribs <christumer@gmail.com>
2023-06-23 00:50:43 -0600

    Adición: SensorError
    Se implementa con el objetivo de generalizar la funcinoalidad de crear un vector de error.
    
    Se modificó Sensor::read_acc para utilizar este struct.

--- sim/src/sensor.rs
+++ sim/src/sensor.rs
@@ -2,6 +2,25 @@ use crate::arenito::*;
 use bevy::prelude::*;
 use rand::{prelude::thread_rng, Rng};
 
+/// This struct's purpose is to generalize "Error Vector" generation.
+/// Since this is a needed step on simulating sensors.
+struct SensorError;
+impl SensorError {
+    const DEFAULT_MIN: f32 = -0.1;
+    const DEFAULT_MAX: f32 = 0.1;
+
+    /// Returns a Vec3 with random values in the range min..max.
+    pub fn vec(min: f32, max: f32) -> Vec3 {
+        Vec3::splat(thread_rng().gen_range(min..max))
+    }
+
+    /// Returns the default (kinda like std::default::Default) vector
+    /// with an error range DEFAULT_MIN..DEFAULT_MAX.
+    pub fn default() -> Vec3 {
+        SensorError::vec(SensorError::DEFAULT_MIN, SensorError::DEFAULT_MAX)
+    }
+}
+
 /// This struct is responsible for the simulations of all sensors related to Arenito.
 /// There's currently only an implementation for an Accelerometer. A Gyroscope is on the way.
 pub struct Sensor;
@@ -15,15 +34,13 @@ impl Sensor {
         // This is the upper bound for the accelerometer readings.
         // Depends entirely on the hardware.
         const ACCELERATION_MAX: f32 = 9.8; // 1g!
-        const ERR_MIN: f32 = -0.1;
-        const ERR_MAX: f32 = 0.1;
 
         // get acceleration value
         // convert to absolute value
         let mut acc = arenito.acc.abs();
 
         // add error
-        acc += Vec3::splat(thread_rng().gen_range(ERR_MIN..ERR_MAX));
+        acc += SensorError::default();
 
         // interpolate each value between [0, 1024],
         // considering that Sensor::ACCELERATION_MAX maps to 1024.
