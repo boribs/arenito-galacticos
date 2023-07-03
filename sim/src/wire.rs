a2598e3b7439eb035ff56cc17a8924ce82d77296
boribs <christumer@gmail.com>
2023-07-02 19:28:38 -0600

    Adición: docstrings

--- sim/src/wire.rs
+++ sim/src/wire.rs
@@ -16,23 +16,35 @@ impl Default for Wire {
 }
 
 impl Wire {
+    /// Creates a new Wire component.
+    /// This is supposed to be passed on spawn and then retrieved
+    /// with a Query.
     pub fn new(start: Vec3, end: Vec3) -> Self {
         Wire { start, end }
     }
 
+    /// Sets the start coordinate of the wire.
+    /// Does not update on the mesh!
     pub fn set_start(&mut self, start: Vec3) {
         self.start = start;
     }
 
+    /// Sets the end coordinate of the wire.
+    /// Does not update on the mesh!
     pub fn set_end(&mut self, end: Vec3) {
         self.end = end;
     }
 
+    /// Updates the mesh of the wire.
+    /// Call this after setting final start and end points.
     pub fn update(&self, mesh: &mut Mesh) {
+        // https://stackoverflow.com/questions/72961896/how-do-i-modify-a-mesh-after-it-has-been-created-in-bevy-rust
+        // i keep forgetting how to get the mesh...
         let points = vec![self.start, self.end];
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, points);
     }
 
+    /// Spawns a Wire on a given position.
     pub fn spawn(
         start: Vec3,
         end: Vec3,
@@ -52,6 +64,7 @@ impl Wire {
         ));
     }
 
+    /// Spawns a Wire on a given position with another component.
     pub fn spawn_unique<C>(
         start: Vec3,
         end: Vec3,
