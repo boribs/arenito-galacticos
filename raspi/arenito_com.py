97000590cf50c74a26f6dad9c46e21853e1e5758
boribs <christumer@gmail.com>
2023-12-27 19:21:59 -0600

    Adición: ArenitoComms
    Capa de abstracción de comunicación con dispositivos.
    Esta clase es responsable de la comunicación directa con el arduino y con la cámara.

---
+++ raspi/arenito_com.py
@@ -0,0 +1,74 @@
+import serial
+from cv2 import VideoCapture
+from cv2.typing import MatLike
+from enum import Enum, auto
+
+class Instruction(Enum):
+    FORWARD = auto()
+    LEFT = auto()
+    RIGHT = auto()
+    BACK = auto()
+    LONG_RIGHT = auto()
+    SCREENSHOT = auto()
+
+INSTRUCTION_MAP = {
+    Instruction.FORWARD: 'a',
+    Instruction.LEFT: 'i',
+    Instruction.RIGHT: 'd',
+    Instruction.BACK: 'r',
+    Instruction.LONG_RIGHT: 'l',
+    Instruction.SCREENSHOT: 'ss',
+}
+
+class ArenitoComms:
+    """
+    Interface between Arenito's AI and other devices.
+    This class is responisble for capturing (raw) images from the camera
+    and communicating with the Arduino board.
+    """
+
+    def __init__(self):
+        self.serial: serial.Serial = None
+        self.video_capture: VideoCapture = None
+
+    def init_video(self, device_index: int = 0):
+        """
+        Initializes the capture device.
+        """
+
+        self.video_capture = VideoCapture(device_index)
+
+    def connect_serial(self, port: str, baudrate: int, timeout: float | None = None):
+        """
+        Establishes serial communication.
+        """
+
+        self.serial = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+
+    def get_image(self) -> MatLike:
+        """
+        Gets the image from the camera.
+        """
+
+        ok, frame = self.video_capture.read()
+        if not ok:
+            raise Exception('Couldn\'t get frame.')
+
+        return frame
+
+    def send_instruction(self, instr: Instruction):
+        """
+        Sends instruction to arduino board through serial interface.
+        """
+
+        # Arduino sends an ok message when its ready to receive an instruction.
+        # Wait for ok message
+        p = self.serial.read()
+
+        # Then send instruction
+        if p:
+            print(f'Enviando {INSTRUCTION_MAP[instr]}::{p}')
+            self.serial.write(bytes(
+                INSTRUCTION_MAP[instr],
+                'utf-8'
+            ))
