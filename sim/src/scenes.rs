27b22caf964e3b47b4d2eedb03dd3b1fe13eef9d
boribs <christumer@gmail.com>
2024-02-16 10:25:56 -0600

    Actualización: Rehacer escena obstáculo

--- sim/src/scenes.rs
+++ sim/src/scenes.rs
@@ -228,21 +228,36 @@ fn spawn_basic_scene_with_cans(
 }
 
 fn spawn_obstacle_scene(
-    _asset_server: Res<AssetServer>,
+    asset_server: Res<AssetServer>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut _can_manager: ResMut<CanManager>,
 ) {
-    let plane_size = 10.0;
+    let texture_handle = asset_server.load("textures/sand_01.png");
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(texture_handle.clone()),
+        reflectance: 0.01,
+        ..default()
+    });
+
+    let plane_size = 15.0;
+    let water_offset = 2.0;
 
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(plane_size).into()),
+        material: material_handle,
+        transform: Transform::from_xyz(0.0, 0.01, 0.0),
+        ..default()
+    });
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(plane_size + water_offset).into()),
         material: materials.add(StandardMaterial {
-            base_color: Color::MIDNIGHT_BLUE,
+            base_color: Color::hex("0080FF").unwrap().into(),
+            reflectance: 0.05,
             ..default()
         }),
-        transform: Transform::from_xyz(0.0, 0.01, 0.0),
         ..default()
     });
 
@@ -261,24 +276,11 @@ fn spawn_obstacle_scene(
 
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-1.01, 10.0, 3.0)
+            transform: Transform::from_xyz(0.01, 20.0, 0.0)
                 .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
             ..default()
         },
         RenderLayers::from_layers(&[0, 1]),
-    ));
-
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Obstacle::get_simple_mesh()),
-            material: materials.add(StandardMaterial {
-                base_color: Color::RED,
-                ..default()
-            }),
-            transform: Transform::from_xyz(4.0, 0.0, 0.0),
-            ..default()
-        },
-        Obstacle,
     ));
 
     commands.spawn((
@@ -288,7 +290,7 @@ fn spawn_obstacle_scene(
                 base_color: Color::RED,
                 ..default()
             }),
-            transform: Transform::from_xyz(4.0, 0.0, 3.0),
+            transform: Transform::from_xyz(5.0, 0.0, 0.0),
             ..default()
         },
         Obstacle,
