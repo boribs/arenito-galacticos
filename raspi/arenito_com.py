8814d11f109adf6deae04449ca214a5d5fc7974b
boribs <christumer@gmail.com>
2024-03-29 21:39:44 -0600

    Actualización: Limpieza de comunicaciones
    Se elimina la comunicación serial y la entrada de video

--- raspi/arenito_com.py
+++ raspi/arenito_com.py
@@ -43,31 +43,31 @@ class ArenitoComms:
     """
 
     def __init__(self, mode: AIMode, args: Namespace):
-        self.serial: SerialInterface | None = None
-        self.video_capture: cv2.VideoCapture | None = None
+        # self.serial: SerialInterface | None = None
+        # self.video_capture: cv2.VideoCapture | None = None
         self.sim_interface: SimInterface | None = None
 
         if mode == AIMode.Simulation:
             self.connect_simulation(args.filename)
-        elif mode == AIMode.Real:
-            self.connect_serial(args.port, args.baudrate, args.timeout)
-            self.init_video()
+        # elif mode == AIMode.Real:
+            # self.connect_serial(args.port, args.baudrate, args.timeout)
+            # self.init_video()
         else:
-            raise Exception(f'No such mode {mode}')
+            raise Exception(f'Unsupported mode {mode}.')
 
-    def init_video(self, device_index: int = 0):
-        """
-        Initializes the capture device.
-        """
+    # def init_video(self, device_index: int = 0):
+    #     """
+    #     Initializes the capture device.
+    #     """
 
-        self.video_capture = cv2.VideoCapture(device_index)
+    #     self.video_capture = cv2.VideoCapture(device_index)
 
-    def connect_serial(self, port: str | None, baudrate: int, timeout: float = 0.0):
-        """
-        Establishes serial communication.
-        """
+    # def connect_serial(self, port: str | None, baudrate: int, timeout: float = 0.0):
+    #     """
+    #     Establishes serial communication.
+    #     """
 
-        self.serial = SerialInterface(port, baudrate, timeout)
+    #     self.serial = SerialInterface(port, baudrate, timeout)
 
     def connect_simulation(self, filename: str):
         """
@@ -81,38 +81,38 @@ class ArenitoComms:
         Gets the image from the front camera.
         """
 
-        if self.video_capture:
-            raise Exception('Real camera input not supported')
+        # if self.video_capture:
+        #     raise Exception('Real camera input not supported')
             # ok, frame = self.video_capture.read()
             # if not ok:
             #     raise Exception('Couldn\'t get frame.')
 
             # return frame
-        else:
-            return self.sim_interface.get_front_frame() # pyright: ignore[reportOptionalMemberAccess]
+        # else:
+        return self.sim_interface.get_front_frame() # pyright: ignore[reportOptionalMemberAccess]
 
     def get_rear_frame(self) -> MatLike:
         """
         Gets the image from the rear camera.
         """
 
-        if self.video_capture:
-            raise Exception('Real camera input not supported')
+        # if self.video_capture:
+        #     raise Exception('Real camera input not supported')
             # ok, frame = self.video_capture.read()
             # if not ok:
             #     raise Exception('Couldn\'t get frame.')
 
             # return frame
-        else:
-            return self.sim_interface.get_rear_frame() # pyright: ignore[reportOptionalMemberAccess]
+        # else:
+        return self.sim_interface.get_rear_frame() # pyright: ignore[reportOptionalMemberAccess]
 
     def get_prox_sensors(self) -> list[int]:
         """
         Returns proximity sensor reads. Only for Sim.
         """
 
-        if self.serial:
-            raise Exception('Proximity sensors not implemented for serial interface')
+        # if self.serial:
+        #     raise Exception('Proximity sensors not implemented for serial interface')
 
         return self.sim_interface.get_proximity_sensor_reads() # pyright: ignore[reportOptionalMemberAccess]
 
@@ -121,9 +121,9 @@ class ArenitoComms:
         Sends instruction to arduino board through serial interface.
         """
 
-        if self.serial:
-            self.serial.send_instruction(instr)
-        elif self.sim_interface:
+        # if self.serial:
+        #     self.serial.send_instruction(instr)
+        if self.sim_interface:
             self.sim_interface.send_instruction(instr)
 
     def dump_cans(self, ammount: int):
@@ -131,9 +131,9 @@ class ArenitoComms:
         Dumps cans.
         """
 
-        if self.serial:
-            raise Exception('Instruction not implemented for Serial interface')
-        elif self.sim_interface:
+        # if self.serial:
+        #     raise Exception('Instruction not implemented for Serial interface')
+        if self.sim_interface:
             self.sim_interface.dump_cans(ammount)
 
 class SerialInterface:
