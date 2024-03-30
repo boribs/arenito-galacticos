b3bd2c98d80bd006a57f1a70fcc88a5dffb38394
boribs <christumer@gmail.com>
2024-03-29 21:50:07 -0600

    Actualización: Interfaz serial como archivo

---
+++ raspi/interfaces/serial_interface.py
@@ -0,0 +1,49 @@
+from arenito_com_conts import *
+from serial import Serial
+import subprocess
+
+class SerialInterface:
+    def __init__(self, port: str | None, baudrate: int, timeout: float = 0.0):
+        self.connect(port, baudrate, timeout)
+
+    def connect(self, port: str | None, baudrate: int, timeout: float):
+        """
+        Establishes serial communication.
+        """
+
+        if port is None: port = SerialInterface.find_port()
+        self.serial = Serial(port=port, baudrate=baudrate, timeout=timeout)
+
+    @staticmethod
+    def find_port() -> str:
+        """
+        Finds out where the Arduino borad is connected. Requires `arduino-cli`.
+        """
+
+        out = subprocess.run(["arduino-cli", "board", "list"], capture_output=True, text=True)
+        ports: list[list[str]] = []
+
+        for line in out.stdout.split('\n')[1:]:
+            if line:
+                line = list(map(lambda n: n.strip(), line.split()))
+                if 'Unknown' not in line:
+                    ports.append(line)
+
+        return ports[0][0]
+
+    def send_instruction(self, instr: Instruction):
+        """
+        Sends instruction to arduino board through serial interface.
+        """
+
+        # Arduino sends an ok message when its ready to receive an instruction.
+        # Wait for ok message
+        p = self.serial.read()
+
+        # Then send instruction
+        if p:
+            print(f'Enviando {INSTRUCTION_MAP[instr]}::{p}')
+            self.serial.write(bytes(
+                INSTRUCTION_MAP[instr],
+                'utf-8'
+            ))
