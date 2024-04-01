f2c1c6c8584b86fce73a2681d42c9f6731f5ec76
boribs <christumer@gmail.com>
2024-03-31 21:07:20 -0600

    Actualización: Esqueleto de código de Arduino

--- arduino/arenito/arenito.ino
+++ arduino/arenito/arenito.ino
@@ -2,16 +2,40 @@ void setup() {
   Serial.begin(115200);
   Serial.setTimeout(0);
 
-  Serial.println("<Arduino is ready>");
+  Serial.println("Arduino ready");
 }
 
+enum InstructionMap {
+  MoveForward = 'a',
+  MoveLeft = 'i',
+  MoveRight = 'd',
+  MoveBack = 'r',
+  MoveLongRight = 'D',
+  RequestProxSensor = 's',
+};
+
 void loop() {
-  while (Serial.available() == 0) {
-    ;
+  while (Serial.available() == 0) { ; }
+
+  char instr = Serial.readString()[0];
+  switch (instr) {
+    case MoveForward:
+      break;
+    case MoveLeft:
+      break;
+    case MoveRight:
+      break;
+    case MoveBack:
+      break;
+    case MoveLongRight:
+      break;
+    case RequestProxSensor:
+      // TODO: Finish this.
+      Serial.println("25,25,100,100,");
+      break;
+    default:
+      break;
   }
 
-  String r = Serial.readString();
-  if (r == "h") {
-    Serial.println("hola");
-  }
+  Serial.println("ok");
 }
