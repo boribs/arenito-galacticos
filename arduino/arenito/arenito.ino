3277842313d25601c1dddf549edbf06735ce848b
boribs <christumer@gmail.com>
2024-04-05 03:04:25 -0600

    Adición: Tapa trasera con limit switches

--- arduino/arenito/arenito.ino
+++ arduino/arenito/arenito.ino
@@ -1,14 +1,17 @@
 #include "ArenitoUtils.h"
 
-const int INSTRUCTION_EXECUTION_TIME = 500; // ms
+const int INSTRUCTION_EXECUTION_TIME = 1000; // ms
 const int MOTOR_PWM_ENABLE = 200;
-const int BACKDOOR_PWM_UP = 130;
+const int BACKDOOR_PWM_UP = 110;
 const int BACKDOOR_PWM_DOWN = 80;
 const int BACKDOOR_TIMEOUT = 1000; // ms
 
 // Don't use pin 13
 IBT2 left = IBT2(12, 11);
 IBT2 right = IBT2(10, 9);
+L298N backdoor = L298N(8, 52, 53);
+LimitSwitch ls_up = LimitSwitch(22);
+LimitSwitch ls_down = LimitSwitch(23);
 
 enum InstructionMap {
     MoveForward = 'a',
@@ -23,6 +26,10 @@ enum InstructionMap {
 void setup() {
     left.setup();
     right.setup();
+    backdoor.setup();
+
+    ls_up.setup();
+    ls_down.setup();
 
     Serial.begin(115200);
     Serial.setTimeout(0);
@@ -56,6 +63,22 @@ void moveBackward() {
     right.stop();
 }
 
+void openBackdoor() {
+    backdoor.clockwise(BACKDOOR_PWM_UP);
+    timeout_repeat(BACKDOOR_TIMEOUT, []() {
+        return ls_up.read() == LOW;
+    });
+    backdoor.stop();
+}
+
+void closeBackdoor() {
+    backdoor.counterClockwise(BACKDOOR_PWM_DOWN);
+    timeout_repeat(BACKDOOR_TIMEOUT, []() {
+        return ls_down.read() == LOW;
+    });
+    backdoor.stop();
+}
+
 void loop() {
     while (Serial.available() == 0) { ; }
 
@@ -67,6 +90,12 @@ void loop() {
 
         case MoveBack:
             moveBackward();
+            break;
+
+        case DumpCans:
+            openBackdoor();
+            delay(1500);
+            closeBackdoor();
             break;
 
         default:
