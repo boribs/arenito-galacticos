68232a23f94175cc2bbebe9891845448fdb884db
boribs <christumer@gmail.com>
2024-04-08 19:03:03 -0600

    Actualización: Facilidad para llamar instrucciones jet.py

--- ai/jet.py
+++ ai/jet.py
@@ -3,18 +3,23 @@ from arenito_com_consts import *
 import argparse, time
 
 parser = argparse.ArgumentParser()
+parser.add_argument('instr', type=str, default=None)
 parser.add_argument('port', nargs='?', type=str, default=None)
 parser.add_argument('baudrate', nargs='?', type=int, default=115200)
 args = parser.parse_args()
+
+instr = eval(f'Instruction.{args.instr}')
 
 jetson_init = time.time()
 ji = JetsonInterface(args, True, True, True)
 jetson_init = time.time() - jetson_init
 
 instruction_time = time.time()
-# ji.send_instruction(Instruction.MoveForward)
-print(
-    ji.get_prox_sensors()
-)
+
+if instr == Instruction.RequestProxSensor:
+    print(ji.get_prox_sensors())
+else:
+    ji.send_instruction(instr)
+
 print('Jetson init time:', jetson_init)
 print('Instruction time:', time.time() - instruction_time)
