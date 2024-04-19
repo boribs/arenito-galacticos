ca76cfe462e0a0d428b2e202e8aadddfcee7b457
boribs <christumer@gmail.com>
2024-04-19 16:46:53 -0600

    Actualización: Ajustes de motores

--- arduino/arenito/arenito.ino
+++ arduino/arenito/arenito.ino
@@ -1,15 +1,15 @@
 #include "ArenitoUtils.h"
 
 const int INSTRUCTION_EXECUTION_TIME = 100; // ms
-const int MOTOR_PWM_ENABLE = 150;
-const int MOTOR_ROT_PWM_ENABLE = 100;
+const int MOTOR_PWM_ENABLE = 250;
+const int MOTOR_ROT_PWM_ENABLE = 230;
 const int BACKDOOR_PWM_UP = 120;
-const int BACKDOOR_PWM_DOWN = 50;
+const int BACKDOOR_PWM_DOWN = 75;
 const int BACKDOOR_TIMEOUT = 1000; // ms
 const int BACKDOOR_EXT_PWM_ENABLE = 130;
 const int BACKDOOR_EXT_TIME_UP = 2000; // ms
 const int BACKDOOR_EXT_TIME_DOWN = 1600; // ms
-const int BRUSH_PWM_ENABLE = 220;
+const int BRUSH_PWM_ENABLE = 200;
 
 // Don't use pin 13
 IBT2 left = IBT2(12, 11);
@@ -23,10 +23,10 @@ LimitSwitch ls_down = LimitSwitch(23);
 //                    trigger, echo
 // ultrasonic front/rear left/right
 // no more front ultrasonic sensors
-// Ultrasonic ufl = Ultrasonic(24, 25);
-// Ultrasonic ufr = Ultrasonic(26, 27);
-Ultrasonic url = Ultrasonic(28, 29);
 Ultrasonic urr = Ultrasonic(30, 31);
+Ultrasonic url = Ultrasonic(25, 24);
+// Ultrasonic url = Ultrasonic(28, 29);
+// Ultrasonic urr = Ultrasonic(30, 31);
 
 IrProx irfl = IrProx(33);
 IrProx irfm = IrProx(36);
@@ -193,7 +193,7 @@ void loop() {
             openBackdoor();
             delay(1500);
             moveForward();
-            delay(300);
+            delay(1000);
             closeBackdoor();
             break;
 
